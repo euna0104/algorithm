2db46dcdbc85fd9c8639ea66db58691caaad0d12
euna0104 <koeuna0104@naver.com>
2023-02-03 10:00:00 +0900

    clear: [lv0] 가까운 수

--- lv.0/closeNum.py
+++ lv.0/closeNum.py
@@ -1,7 +1,9 @@
 def solution(array, n):
-    answer = 0
-    answer2 = []
+    array.sort() #오름차수s
+    answer = array[0] #가장 작은 수
+    num = abs(n - array[0]) #주어진 수에서 array의 가장 작은수 빼기 
     for i in array:
-        answer2.append(abs(n - i))
-    num = answer2.index(min(answer2))
-    return array[num]
+        if num > abs(n - i): #num 보다 abs(n - i)가 더 작으면
+            num = abs(n - i) #num은 abs(n - i)가 된다
+            answer = i answer는 i
+    return answer
