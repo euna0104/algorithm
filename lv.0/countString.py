4f5b8bcda96ac8aed31ac9e2c7590c685521f809
euna0104 <koeuna0104@naver.com>
2023-03-17 10:00:00 +0900

    cleat: [lv0] 문자열이 몇 번 등장하는지 세기

--- lv.0/countString.py
+++ lv.0/countString.py
@@ -1,7 +1,7 @@
 def solution(myString, pat):
     answer = 0
     for i in range(len(myString)):
-        if pat in myString[i:]:
+        if myString[i:i+len(pat)] == pat:
             answer += 1
     return answer
   
