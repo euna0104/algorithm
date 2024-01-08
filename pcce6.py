17ad5f2093d3d2849f921a6db3b80e5c307c2285
euna0104 <koeuna0104@naver.com>
2024-01-08 16:41:14 +0900

    clear: [PCCE6] 가채점

--- pcce6.py
+++ pcce6.py
@@ -2,8 +2,8 @@ def solution(numbers, our_score, score_list):
     result= []
     for i in range(len(numbers)):
         if our_score[i] == score_list[numbers[i]-1]:
-            return result.append("Same")
+            result.append("Same")
         else:
-            return result.append("Different")
+            result.append("Different")
            
     return result
