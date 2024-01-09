23a85723c98d96872c5440caba963815f1736465
euna0104 <koeuna0104@naver.com>
2024-01-09 12:26:55 +0900

    clear: [lv.0] 수 조작하기

--- controlNum1.py
+++ controlNum1.py
@@ -1,15 +1,31 @@
 # integer를 넣어야함
 def solution(n, control):
-    n = 0
-    list(control)
-    for i in control:
+    for i in range (len(control)):
         if control[i] == "w":
             n += 1
         elif control[i] == "s":
             n -= 1
-        elif control[i] == "n":
+        elif control[i] == "d":
+            n += 10
+        elif control[i] == "a":
+            n -= 10
+    return n
+            
+'''
+control의 길이만큼 해줄 것.
+그리고 들여쓰기로 인해 제대로된 n이 출력 안됨.
+문자열로 해결 법
+
+def solution(n, control):
+    for i in control: # 문자열로 해결
+        if i == "w":
+            n += 1
+        elif i == "s":
+            n -= 1
+        elif i == "d":
             n += 10
         else:
             n -= 10
-        return n
-            
+    return n
+        
+'''
