e17c5c4f1178cb69999214a814b9a356f210c219
euna0104 <koeuna0104@naver.com>
2024-01-10 15:06:23 +0900

    clear: [lv.0] 수 조작하기2

--- controlNum2.py
+++ controlNum2.py
@@ -1,5 +1,16 @@
-def solution(numLog): # numLog는 배열
-    result = 'w'
-    for i in result:
-        if i == "w":
-            result.append
+def solution(numLog):
+    result = ''
+    for i in range (1, len(numLog)):
+        if numLog[i]- numLog[i-1] == 1:
+            result += 'w'
+        elif numLog[i]- numLog[i-1] == -1:
+            result += 's'
+        elif numLog[i]- numLog[i-1] == 10:
+            result += 'd'
+        elif numLog[i]- numLog[i-1] == -10:
+            result += 'a'
+    return result        
+        
+'''
+
+'''
