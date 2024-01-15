f32e04f2827706ecd4db502a4cac0eed9d80c04e
euna0104 <koeuna0104@naver.com>
2024-01-15 15:44:38 +0900

    clear: [lv.0] 머쓱이보다 키 큰사람

--- moreThanHeight.py
+++ moreThanHeight.py
@@ -1,7 +1,7 @@
 def solution(array, height):
-    reArray = 0
-    for i in range (len(array)):
-        if (height < array[i]):
-            reArray += 1
-    return reArray 
+    sum = 0
+    for i in array: # array 배열 안에서
+        if i > height: # i의 수가 주어진 height 보다 크다면
+            sum += 1 # sum에 1을 더한다.
+    return sum
     
