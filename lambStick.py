47665d24fcd10dca1e63dd63a1d53d9b789d4842
euna0104 <koeuna0104@naver.com>
2024-01-15 15:10:37 +0900

    clear: [lv.0] 양꼬치

--- lambStick.py
+++ lambStick.py
@@ -1,6 +1,11 @@
 def solution(n, k):
     sum = (n*12000)+(k*2000)
-    if n % 10:
-        return sum - ((n/10)*2000)
+    if n >= 10 :
+        return sum - (int(n/10)*2000)
     else:
         return sum
+
+'''
+int로 바꿔주는 것 꼭 생각하기.
+소수점자리 날려주는 거것 생각하기 
+'''
