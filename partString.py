650071ede9fce443471f6bd2e563c03e4913cc15
euna0104 <koeuna0104@naver.com>
2024-01-15 13:45:53 +0900

    clear: [lv.0] 부분 문자열 이어 붙여 문자열 만들기

--- partString.py
+++ partString.py
@@ -1,3 +1,22 @@
 def solution(my_strings, parts):
-    print(my_strings)
+    answer = ''
+    for i in range (len(my_strings)):
+        answer += my_strings[i][parts[i][0]:parts[i][1]+1]
+    return answer
+      
+'''
+2차원 배열로 생각해야하는 부분인가?
+난 직관적인 코드가 좋음
+
+def solution(my_strings, parts):
+    answer = ""
+    for i in range(len(parts)):
+        answer += (my_strings[i][parts[i][0]:parts[i][1] + 1])
+    return answer
     
+def solution(my_strings, parts):
+    result = ''
+    for i in range(len(my_strings)):
+        result += my_strings[i][parts[i][0] : parts[i][1] + 1]
+    return result
+'''
