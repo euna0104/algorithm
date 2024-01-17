8c186e83c32e960277def119d799ca54062253a7
euna0104 <koeuna0104@naver.com>
2024-01-17 16:02:19 +0900

    clear: [lv.0] 순서쌍의개수

--- lv.0/orderedPair.py
+++ lv.0/orderedPair.py
@@ -1,9 +1,9 @@
 def solution(n):
     answer = 0
     for i in range(1, n+1):
-        for j in range(1, n+1):
-            if i * j == n:
-                answer += 1
+        j = n % i
+        if j == 0:
+            answer += 1
     return answer
 
-# 이렇게 해결하면 시간이 오래걸리는 문제가 존재한다.
+# for문을 한번만 작성해주기. 시간복잡도 해결 성공.
