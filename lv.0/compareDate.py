866dd492f7c2244aa40eaf456b83e9fb7cb7b0a3
euna0104 <koeuna0104@naver.com>
2024-01-30 10:00:00 +0900

    clear: [lv0] 특별한 이차원 배열1

---
+++ lv.0/compareDate.py
@@ -0,0 +1,19 @@
+def solution(date1, date2):
+    #연도 비교( 연도부터 다른 경우)
+    if date1[0] != date2[0]:
+        if date1[0] < date2[0]:
+            return 1
+        else:
+            return 0
+    # 월 비교 (연도가 같다)
+    else:
+        if date1[1] < date2[1]:
+            return 1
+        elif date1[1] == date2[1]:
+            if date1[2] < date2[2]:
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+        
