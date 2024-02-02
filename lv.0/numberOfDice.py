9c4a3939000c267dfd094a9394c8866f67408dd9
euna0104 <koeuna0104@naver.com>
2024-02-02 11:55:10 +0900

    clear: [lv.0] 주사위의 개수

--- lv.0/numberOfDice.py
+++ lv.0/numberOfDice.py
@@ -4,4 +4,16 @@ def solution(box, n):
 '''
 box엔 리스트가 들어있음
 구하는법은 가로.세로.높이 몫을 곱하는 것이다.
+
+파이썬 함수 중 math.prod() 를 사용하는 방법도 있다.
+math.prod()는 ()안에 있는 모든 iterable 요소들의 곱을 구한다.
+
+
+import math
+
+def solution(box, n):
+    return math.prod(map(lambda v: v//n, box))
+    
+box안에 있는 모든 값 v들을 n으로 나눈 값의 몫을 모두 곱하는 것이다.
+
 '''
