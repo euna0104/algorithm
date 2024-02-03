f82808613d1b8d64e41bbe162c1bb26ec82bf665
euna0104 <koeuna0104@naver.com>
2024-02-03 10:00:00 +0900

    [lv0] clear: 문자열 뒤집기

--- lv.0/reverseStr.py
+++ lv.0/reverseStr.py
@@ -1,19 +1,6 @@
-def solution(my_string, s, e):
-    a = ''
-    b = ''
-    c = ''
-    my_str = list(my_string)
-    for i in my_str[0:s:]:
-        a += i
-    for i in my_str[e:s-1:-1]:
-        b += i
-    for i in my_str[e+1::]:
-        c += i
-    return a+b+c
-        
+def solution(my_string, s, e):    
+    return my_string[:s]+my_string[s:e+1][::-1]+my_string[e+1:]
     
 '''
-세덩이로 나누겠다.
-앞 a, 중간 b, 끝 c
-증간은 revers 해주겠다
+for문 보다 slice로 해주는 것이 더 좋다.
 '''
