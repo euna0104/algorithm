8862df5ede575f30158f7901e09d8a58c9256ecb
euna0104 <koeuna0104@naver.com>
2024-02-06 16:53:46 +0900

    clear: [lv.0] 약수구하기

--- lv.0/divisorNumber.py
+++ lv.0/divisorNumber.py
@@ -1,6 +1,11 @@
 def solution(n):
     answer = []
-    for i in n:
+    for i in range(1, n+1):
         if n % i == 0:
             answer.append(i)
     return answer
+
+'''
+약수를 구할 때는 1부터 시작하고, n+1(끝)까지 실행해야하는
+range를 정해주어야 한다는 것을 알았다.
+'''
