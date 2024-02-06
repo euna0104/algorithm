2edd65bc0f7e22376c0a4b9cf95834ef756287b1
euna0104 <koeuna0104@naver.com>
2024-02-06 15:49:06 +0900

    cleart: [lv.0] 인덱스 바꾸기

--- lv.0/changeIndex.py
+++ lv.0/changeIndex.py
@@ -1,10 +1,11 @@
 def solution(my_string, num1, num2):
-    answer = ''
-    for i in my_string:
-        if i == num1:
-            answer += my_string.replace(my_string[num2], my_string[num1])
-        elif i == num2:
-            answer += my_string.replace(my_string[num1], my_string[num2])
-        else:
-            answer += i
-    return answer
+    s = ''
+    my_string = list(my_string)
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+    return s.join(my_string)
+
+'''
+1. num1번째, num2번째 접근을 위해 my_string을 list형태로 변경
+2. num1번째와 num2번째의 순서를 변경하여 정의한다.
+3. 바꾼 문자열을 join을 사용하여 s에 붙인다. 그리고 다시 string형태로 만든다.
+'''
