1c24a4ad94c415d013b2f2b2cefd7f57d70d3910
euna0104 <koeuna0104@naver.com>
2024-02-09 23:31:59 +0900

    clear: [lv.0] 배열 만들기3

---
+++ lv.0/makeList3.py
@@ -0,0 +1,10 @@
+def solution(arr, intervals):
+    answer = []
+    # return answer
+    # return intervals[0][1]
+    for i in arr[intervals[0][0]:intervals[0][1]+1]:
+        answer.append(i)
+    for i in arr[intervals[1][0]:intervals[1][1]+1]:
+        answer.append(i)
+    return answer
+    
