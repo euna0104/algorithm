f449c7e171b5fa7ef2643e27412150b442388a84
euna0104 <koeuna0104@naver.com>
2024-02-13 10:00:00 +0900

    [lv0} clear: 수열과 구간 쿼리 1

--- lv.0/seqSec1.py
+++ lv.0/seqSec1.py
@@ -1,13 +1,14 @@
 def solution(arr, queries):
-    #answer = []
-    #return answer
-    queries2 = sum(queries, [])
-    for i in queries2:
-        arr[i] = arr[i]+1
+    for l,r in queries:
+        for i in range(l, r+1):
+            arr[i] += 1
     return arr
         
-
 '''
 queries에 있는 숫자에 1을 더한다.
 sum(queries, []) -> 묶여있는 리스트들 해체하기
+
+ 순서대로 s ≤ i ≤ e인 모든 i에 대해 arr[i]에 1을 더합니다.
+ -> 생각해보니 해체가 중요한게 아니였다.
+ 이 수열 구간 안에 있는 애들에게 1을 더하는 것
 '''
