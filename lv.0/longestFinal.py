4115207f38a18cbf07490bb0be16e9ec4d9c567c
euna0104 <koeuna0104@naver.com>
2024-02-15 10:00:00 +0900

    [lv0} clear: 특정 문자열로 끝나는 가장 긴부분

---
+++ lv.0/longestFinal.py
@@ -0,0 +1,9 @@
+def solution(myString, pat):
+    answer = ''
+    #return answer
+    for i in myString:
+        if i == pat:
+            break
+        else:
+            break
+    return answer
