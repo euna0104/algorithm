d3865f567ba824436c509f2f5509031447bdb487
euna0104 <koeuna0104@naver.com>
2024-02-16 10:00:00 +0900

    clear: [lv0] 2의영역

--- lv.0/range2.py
+++ lv.0/range2.py
@@ -1,15 +1,5 @@
 def solution(arr):
     if 2 not in arr:
         return [-1]
-    else:
-        answer = ""
-        answer2 = []
-        for i in arr:
-            answer += str(i)
-        first = answer.index("2")
-        final = answer.rindex("2")
-        return final
-        for i in arr[first:final+1:1]:
-            answer2.append(i)
-        return answer2
+    return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
         
