208af670bec0db202d7c9727f4a12fb2130440e3
euna0104 <koeuna0104@naver.com>
2024-02-21 10:00:00 +0900

    mid: [lv0] 배열의 길이를 2의 거듭제곱으로 만들기

--- lv.0/makeLen2.py
+++ lv.0/makeLen2.py
@@ -1,13 +1,13 @@
 def solution(arr):
-    #answer = []
-    #return answer
     if len(arr) == 2:
         return arr
     elif len(arr) % 4 == 0:
         return arr
     else:
+        for i in range(len(arr) - (len(arr)//2)-1):  # 정수 나눗셈으로 변경합니다.
+            arr.append(0)
         return arr
-      
+
 '''
-예외처리를 못하고 있음
+예외처리는 하였으나 테스트케이스를 통과 못한 구간이 있다.
 '''
