e1b38d9b38acc5084e5db664542086f6214e1a2d
euna0104 <koeuna0104@naver.com>
2024-02-27 16:25:35 +0900

    clear: [lv.0] 합성수 찾기

--- lv.0/compositeNum.py
+++ lv.0/compositeNum.py
@@ -1,6 +1,13 @@
 def solution(n):
+    def count_divisors(n):
+        count = 0
+        for i in range(1, n + 1):
+            if n % i == 0:
+                count += 1
+        return count
+    
     answer = 0
-    for i in range(4, n+1):
-        if (i % 2 == 0) or (i % 3 == 0) or ( i == 25) or ( i == 49):
+    for i in range(4, n + 1):
+        if count_divisors(i) >= 3:
             answer += 1
     return answer
