b2ec71c465be91d89e0e5cc37e13891bb7f03b87
euna0104 <koeuna0104@naver.com>
2024-03-02 10:00:00 +0900

    clear: [lv0] 특정한 문자를 대문자로 바꾸기

--- lv.0/changeUpper.py
+++ lv.0/changeUpper.py
@@ -1,6 +1,3 @@
 def solution(my_string, alp):
-    if alp in my_string:
-        point = my_string.index(alp)
-        return (my_string[point].upper() + my_string[point+1:])
-    else:
-        return my_string
+    return my_string.replace(alp, alp.upper())
+  
