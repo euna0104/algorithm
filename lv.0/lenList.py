0948831fc30c49d7e8ee512a68ffb6d1bed848d1
euna0104 <koeuna0104@naver.com>
2024-03-07 10:00:00 +0900

    clear: [lv0] 배열의 길이에 따라 다른 연산하기

--- lv.0/lenList.py
+++ lv.0/lenList.py
@@ -1,11 +1,10 @@
 def solution(arr, n):
-    answer = []
-    if len(arr) % 2 == 1:
-        for i in range(0,len(arr)+1,1):
+    for i, val in enumerate(arr):
+        if len(arr) % 2 == 0:
             if i % 2 == 1:
-                answer.append(arr[i])
-            else:
-                answer.append(arr[i]+n)
-    else:
-        return 0
-        
+                arr[i] += n
+        else:
+            if i % 2 == 0:
+                arr[i] += n
+    return arr
+                
