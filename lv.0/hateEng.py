cde0703124c3212f7a0dffa83a0cd969c9be9d8e
euna0104 <koeuna0104@naver.com>
2024-03-12 10:00:00 +0900

    clear: [lv0] 영어가 싫어요

---
+++ lv.0/hateEng.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    #numbers.replace('one','1')
+    #return numbers
+    if 'one' in numbers:
+        numbers.replace('one','1')
+    return numbers
+                    
+'''
+replace가 되지 않는 문제 발생
+'''
